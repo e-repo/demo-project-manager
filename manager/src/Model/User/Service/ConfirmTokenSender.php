0d7f39215b721c23c587e1edeedbf5a1d25b686b
Evgeny <postmail.evg@mail.ru>
2020-09-05 22:36:58 +0300

    Added sign-up confirm page

--- manager/src/Model/User/Service/ConfirmTokenSender.php
+++ manager/src/Model/User/Service/ConfirmTokenSender.php
@@ -5,11 +5,54 @@ declare(strict_types=1);
 namespace App\Model\User\Service;
 
 use App\Model\User\Entity\User\Email;
+use Twig\Environment;
 
 class ConfirmTokenSender
 {
-    public function send(Email $email, $token): void
-    {
+    /**
+     * @var \Swift_Mailer
+     */
+    private $mailer;
+    /**
+     * @var Environment
+     */
+    private $twig;
+    /**
+     * @var array
+     */
+    private $from;
 
+    /**
+     * ConfirmTokenSender constructor.
+     * @param \Swift_Mailer $mailer
+     * @param Environment $twig
+     * @param array $from
+     */
+    public function __construct(\Swift_Mailer $mailer, Environment $twig, array $from)
+    {
+        $this->mailer = $mailer;
+        $this->twig = $twig;
+        $this->from = $from;
+    }
+
+    /**
+     * @param Email $email
+     * @param string $token
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function send(Email $email, string $token): void
+    {
+        $message = (new \Swift_Message('Sign up confirmation.'))
+            ->setFrom($this->from)
+            ->setTo($email->getValue())
+            ->setBody($this->twig->render('mail/user/signup.html.twig', [
+                'token' => $token,
+            ]), 'text/html');
+
+        if (!$this->mailer->send($message)) {
+            throw new \RuntimeException('Unable to send message');
+        }
     }
 }
