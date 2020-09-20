17688973c2c67fc76b60725bb4b4c28a381f1346
Evgeny <postmail.evg@mail.ru>
2020-09-20 19:52:52 +0300

    Add logout link

--- manager/src/Controller/AuthController.php
+++ manager/src/Controller/AuthController.php
@@ -21,4 +21,14 @@ class AuthController extends AbstractController
 
         return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
+
+    /**
+     * @Route("/logout", name="app_logout", methods={"GET"})
+     * @throws \Exception
+     */
+    public function logout()
+    {
+        // controller can be blank: it will never be executed
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
+    }
 }
